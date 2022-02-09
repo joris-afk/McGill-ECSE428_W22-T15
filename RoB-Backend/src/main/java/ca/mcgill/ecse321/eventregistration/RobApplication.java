fe4e469e41d60718fa8051ea610ce3e6d4ad5ed3
joris-afk <joris.ah-kane@mail.mcgill.ca>
2022-02-08 19:07:08 -0500

    setup phase

---
+++ RoB-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/RobApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobApplication.class, args);
+	}
+
+}
