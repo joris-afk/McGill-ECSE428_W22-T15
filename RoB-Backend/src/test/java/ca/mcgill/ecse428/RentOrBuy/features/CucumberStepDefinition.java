2e9a12c9f236635898461e0679233d1aa2cbdbd3
YuJ-Li <liyujinjin@hotmail.com>
2022-02-10 16:14:44 -0500

    cucumber
    fixing project build

--- RoB-Backend/src/test/java/ca/mcgill/ecse428/RentOrBuy/features/CucumberStepDefinition.java
+++ RoB-Backend/src/test/java/ca/mcgill/ecse428/RentOrBuy/features/CucumberStepDefinition.java
@@ -1,28 +1,29 @@
 package ca.mcgill.ecse428.RentOrBuy.features;
 
-//import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.When;
-//import io.cucumber.java.en.Then;
-//import static org.junit.Assert.*;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.*;
 
 public class CucumberStepDefinition {
 
-//	@Given("today is Sunday")
-//	public void today_is_sunday() {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new io.cucumber.java.PendingException();
-//	}
-//
-//	@When("I ask whether it is Friday yet")
-//	public void i_ask_whether_it_is_friday_yet() {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new io.cucumber.java.PendingException();
-//	}
-//
-//	@Then("I should be told {string}")
-//	public void i_should_be_told(String string) {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new io.cucumber.java.PendingException();
-//	}
-//	
+	@Given("today is Sunday")
+	public void today_is_sunday() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("I ask whether it is Friday yet")
+	public void i_ask_whether_it_is_friday_yet() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("I should be told {string}")
+	public void i_should_be_told(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	
 }
