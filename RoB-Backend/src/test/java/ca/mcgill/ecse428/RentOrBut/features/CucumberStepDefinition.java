a050b41a6aa7eab7669696bef8b892e4a55bc70a
Chun160712 <chun160712@gmail.com>
2022-02-10 16:01:51 -0500

    Cucumber setup

---
+++ RoB-Backend/src/test/java/ca/mcgill/ecse428/RentOrBut/features/CucumberStepDefinition.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse428.RentOrBut.features;
+
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.When;
+//import io.cucumber.java.en.Then;
+//import static org.junit.Assert.*;
+
+public class CucumberStepDefinition {
+
+//	@Given("today is Sunday")
+//	public void today_is_sunday() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@When("I ask whether it is Friday yet")
+//	public void i_ask_whether_it_is_friday_yet() {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//
+//	@Then("I should be told {string}")
+//	public void i_should_be_told(String string) {
+//	    // Write code here that turns the phrase above into concrete actions
+//	    throw new io.cucumber.java.PendingException();
+//	}
+//	
+}
