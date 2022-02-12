6cf23f905533998b66791a64ee088846dba60357
joris-afk <joris.ah-kane@mail.mcgill.ca>
2022-02-12 14:41:46 -0500

    Update ItemInCartDto.java

--- RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/dto/ItemInCartDto.java
+++ RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/dto/ItemInCartDto.java
@@ -1,5 +1,22 @@
 package ca.mcgill.ecse428.RentOrBuy.dto;
 
 public class ItemInCartDto {
+    private ItemDto item;
+	private int quantity;
+	private String size;
 
+    public ItemInCartDto(){
+    }
+
+    public ItemDto getItem(){
+        return item;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public String getSize(){
+        return size;
+    }
 }
