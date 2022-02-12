d0d4dbc02e160e4fe087fcfc6480a2a8acbf7f6c
joris-afk <joris.ah-kane@mail.mcgill.ca>
2022-02-12 15:02:19 -0500

    trying to fix build issue

--- RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/dao/ItemInCartRepository.java
+++ RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/dao/ItemInCartRepository.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse428.RentOrBuy.dao;
 
 import org.springframework.data.repository.CrudRepository;
-
+/*
 import ca.mcgill.ecse428.RentOrBuy.model.Item;
 import ca.mcgill.ecse428.RentOrBuy.model.ItemInCart;
 
@@ -10,3 +10,4 @@ public interface ItemInCartRepository extends CrudRepository<ItemInCart, Item>{
     ItemInCart findItemInCartbyItem(Item item);
 
 }
+*/
