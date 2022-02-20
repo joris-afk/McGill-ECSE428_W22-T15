844741f5af9712fa062d4766dda48be6f33f6455
joris-afk <joris.ah-kane@mail.mcgill.ca>
2022-02-19 19:05:07 -0500

    fixed application user persistence test

--- RoB-Backend/src/test/java/ca/mcgill/ecse428/RentOrBuy/dao/TestApplicationUserPersistence.java
+++ RoB-Backend/src/test/java/ca/mcgill/ecse428/RentOrBuy/dao/TestApplicationUserPersistence.java
@@ -21,16 +21,24 @@ import ca.mcgill.ecse428.RentOrBuy.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestApplicationUserPersistence {
-    /*
+    
     @Autowired
 	private ApplicationUserRepository AUrepository;
 	
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
 	@AfterEach
 	public void clearDataBase() {
 		AUrepository.deleteAll();
+        cartRepository.deleteAll();
+        itemRepository.deleteAll();
 	}
 
     @Test
@@ -43,8 +51,24 @@ public class TestApplicationUserPersistence {
 	    String password = "qwerty123";
 	    String fullname = "Arnold Schwartz";
 	    String address = "THE LAND OF FREEDOM";
+
 	    Cart cart = new Cart();
+        cart.setCartId(12345);
+
+        cartRepository.save(cart);
+
 	    List<Item> items = new ArrayList<Item>();
+        Item i1 = new Item();
+        i1.setName("flippers");
+        Item i2 = new Item();
+        i2.setName("gloves");
+        Item i3 = new Item();
+        i3.setName("gerbil (illegal)");
+        items.add(i1);
+        items.add(i2);
+        items.add(i3);
+        itemRepository.saveAll(items);
+
 
         aAppUser.setUsername(username);
         aAppUser.setPassword(password);
@@ -64,8 +88,11 @@ public class TestApplicationUserPersistence {
         assertEquals(password, aAppUser.getPassword());
         assertEquals(fullname, aAppUser.getFullname());
         assertEquals(address, aAppUser.getAddress());
-        assertEquals(cart, aAppUser.getCart());
-        assertEquals(items, aAppUser.getItems());
+        assertEquals(cart.getCartId(), aAppUser.getCart().getCartId());
+        
+        for (int i=0;i<items.size();i++){
+            assertEquals(items.get(i).getName(), aAppUser.getItems().get(i).getName());
+        }
     }
-    */
+    
 }
