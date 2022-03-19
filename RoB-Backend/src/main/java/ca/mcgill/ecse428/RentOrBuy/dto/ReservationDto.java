43a8ca554a807510103ff52b14ff614f24f45563
joris-afk <joris.ah-kane@mail.mcgill.ca>
2022-03-19 01:08:10 -0400

    Reservation backend for adding

--- RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/dto/ReservationDto.java
+++ RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/dto/ReservationDto.java
@@ -2,13 +2,34 @@ package ca.mcgill.ecse428.RentOrBuy.dto;
 
 public class ReservationDto {
     private ItemDto item;
+    private int quantity;
     private ApplicationUserDto user;
-    private int reservationId;
+    private long reservationId;
 
     public ReservationDto(){}
-    public ReservationDto(int reservationId, ApplicationUserDto user,ItemDto item){
+    public ReservationDto(long reservationId, ApplicationUserDto user,ItemDto item){
         this.reservationId=reservationId;
         this.user=user;
         this.item=item;
+        this.quantity=1;
+    }
+
+    public ReservationDto(long reservationId, ApplicationUserDto user,ItemDto item, int quantity){
+        this.reservationId=reservationId;
+        this.user=user;
+        this.item=item;
+        this.quantity=quantity;
+    }
+
+    public ItemDto getItem(){
+        return item;
+    }
+
+    public ApplicationUserDto getUser(){
+        return user;
+    }
+
+    public long getReservationId(){
+        return reservationId;
     }
 }
--- RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/service/ReservationService.java
+++ RoB-Backend/src/main/java/ca/mcgill/ecse428/RentOrBuy/service/ReservationService.java
@@ -19,12 +19,36 @@ public class ReservationService {
     private ReservationRepository reservationRepository;
 
     @Transactional
-    public Reservation getReservationByReservationId(int reservationId){
+    public Reservation createReservation(Item item, ApplicationUser user, int quantity, long reservationId){
+        if (item == null){
+            throw new IllegalArgumentException("Please specify an item");
+        }
+        if (user == null){  //hypothetically this can't happen unless the user is not logged in
+            throw new IllegalArgumentException("Please specify a buyer");
+        }
+        if (quantity < 0){
+            throw new IllegalArgumentException("Please specify a positive quantity");
+        }
+        
+        Reservation aReservation = new Reservation();
+        aReservation.setItem(item);
+        aReservation.setQuantity(quantity);
+        aReservation.setUser(user);
+        aReservation.setReservationId(reservationId);
+        reservationRepository.save(aReservation);
+        return aReservation;
+    }
+
+    @Transactional
+    public Reservation getReservationByReservationId(long reservationId){
         Reservation aRerservation = reservationRepository.findReservationByReservationId(reservationId);
         return aRerservation;
     }
+
     @Transactional
     public void deleteReservation(Reservation reservation){
         reservationRepository.delete(reservation);
     }
+
+    //NO UPDATE FOR NOW, JUST DELETE AND ADD AGAIN
 }
